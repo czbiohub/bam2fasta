67b04030ddfabb779c0262bb785255e3cd2f8bd6
pranathi.vemuri <pranathi93.vemuri@gmail.com>
2020-04-01 21:38:15 -0700

    install pandas

--- setup.py
+++ setup.py
@@ -54,7 +54,8 @@ SETUP_METADATA = \
                              "pathos==0.2.5",
                              "pysam==0.15.3",
                              "tqdm==4.36.1",
-                             "numpy==1.16.1"],
+                             "numpy==1.16.1",
+                             "pandas==0.25.1"],
         "setup_requires": [
             "setuptools>=38.6.0",
             "setuptools_scm",
